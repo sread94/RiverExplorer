97003a57698f68fb2de90a6460ed31818c63dd5e
sread94 <sread94@gmail.com>
2015-10-04 00:09:18 -0400

    Check Tree Validity
    implemented the check tree function.

--- TreeVisualization/DisplayTree.js
+++ TreeVisualization/DisplayTree.js
@@ -27,13 +27,28 @@ function readTree(){
 	var tempNode;
 	for(var i = 0; i<data.numNodes; i++){
 		tempNode = new Node(data.nodeIds[i], data.vals[i],
-			data.coords[i]);
+			data.coords[i], getNeighbors(data.probBtwNodes, data.nodeIds[i]));
 		nodeList[i] = tempNode;
 	}
 	
 	var theTree = new Tree(data.numNodes, nodeList,
 		data.probBtwNodes);
-	drawTree(theTree);
+	if(checkTree(theTree)){
+		drawTree(theTree);
+	}
+	else{
+		window.alert("the tree you provided was not valid");
+	}
+}
+
+function getNeighbors(probabilities, nodeId){
+	var neighbors = [];
+	for(var i = 0; i < probabilities.length; i++){
+		if(probabilities[i][0] === nodeId){
+			neighbors.push(probabilities[i][1]);
+		}
+	}
+	return neighbors;
 }
 
 /**
@@ -42,7 +57,58 @@ function readTree(){
 	tree (i.e. no loops, bidirected edges)
 */
 function checkTree(tree){
+	//if odd number of probabilities the tree is not bidirected
+	if(tree.edgeProbs.length%2 ===1){
+		return false;
+	}
 
+	//if any pair of nodes a b has a probability
+	//from a to b it must have a probability from b to a
+	//if not return false
+	for(var i = 0; i < tree.edgeProbs.length; i++){
+		var startNode = tree.edgeProbs[i][0];
+		var endNode = tree.edgeProbs[i][1];
+		var pairExists = false;
+		for(var j = 0; j < tree.edgeProbs.length; j++){
+			var otherStart = tree.edgeProbs[j][0];
+			var otherEnd = tree.edgeProbs[j][1];
+			if(startNode === otherEnd
+				&& endNode === otherStart){
+				pairExists = true;
+			}
+		}
+		if(!pairExists){
+			return false;
+		}
+	}
+
+	//check for loops
+	//breadth first search should never see
+	//the same node twice
+	var visitedNodes = [];
+	var queue = [ [tree.nodeList[0].nodeId, -1] ];
+	while(visitedNodes.length < tree.nodeList.length){
+		var curNodeInfo = queue.pop();
+		var curNode = curNodeInfo[0];
+		var curNodeParent = curNodeInfo[1];
+
+		visitedNodes.push(curNode.nodeId);
+
+		for(var l = 0; l < tree.getNodeById(curNode).neighbors.length; l++){
+			var curNeighbor = tree.getNodeById(curNode).neighbors[l];
+			if(curNeighbor !== curNodeParent){
+				window.alert("Here");
+				if(curNeighbor in visitedNodes){
+					return false;
+				}
+				queue.push([tree.getNodeById(curNode).neighbors[l], 
+					curNode]);
+			}
+		}
+
+	}
+
+	return true;
 }
 
 /**
@@ -114,8 +180,9 @@ function Tree(numNodes, nodeList, edgeProbs){
 /**
 	Node class
 */
-function Node(theId, val, coords){
+function Node(theId, val, coords, neighbors){
 	this.nodeId = theId;
 	this.val = val;
 	this.coordinates = coords;
+	this.neighbors = neighbors;
 }
